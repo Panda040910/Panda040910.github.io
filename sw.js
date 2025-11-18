2523f2923123928cc84cb3677b5b325543ff4c8f
GitHub Panda040910 <Github dezhipan27@gmail.com>
2025-11-18 12:20:43 +0800

    Site updated: 2025-11-18 12:20:43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1763437328146";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1763439639137";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
